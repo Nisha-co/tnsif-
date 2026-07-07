e01661318144e89b413a96eda56daa34c0a56613
Nisha D <nishagowdayd@gmail.com>
2026-07-07 20:14:56 +0530

    Add files via upload

---
+++ ShoppingmallItemmanagement/shoopingmallprojectmanagement/src/main/java/com/example/shoopingmallprojectmanagement/ShoopingmallprojectmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.shoopingmallprojectmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoopingmallprojectmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoopingmallprojectmanagementApplication.class, args);
+	}
+
+}
---
+++ ShoppingmallItemmanagement/shoopingmallprojectmanagement/src/test/java/com/example/shoopingmallprojectmanagement/ShoopingmallprojectmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoopingmallprojectmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoopingmallprojectmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
